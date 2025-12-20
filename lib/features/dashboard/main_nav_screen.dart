234bcdec78b7790090bc07a60f705873a9adbd35
bhumialokesh96-netizen <bhumialokesh96@gmail.com>
2025-12-20 17:05:22 +0530

    update main_nav_screen.dart

--- lib/features/dashboard/main_nav_screen.dart
+++ lib/features/dashboard/main_nav_screen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:winmate/features/home/home_dashboard.dart';
 import 'package:winmate/features/mining/mining_dashboard.dart';
 import 'package:winmate/features/invite/invite_screen.dart';
-import 'package:winmate/features/profile/profile_screen.dart';
-
+import 'package:winmate/features/profile/profile_screen.dart'; // <--- We keep this
 
 class MainNavScreen extends StatefulWidget {
   const MainNavScreen({super.key});
@@ -13,31 +12,24 @@ class MainNavScreen extends StatefulWidget {
 }
 
 class _MainNavScreenState extends State<MainNavScreen> {
-  // --- THIS VARIABLE WAS MISSING BEFORE ---
   int _selectedIndex = 0;
 
-  // --- THE SCREENS LIST ---
   final List<Widget> _screens = [
-  const HomeDashboard(),   
-  const MiningDashboard(), 
-  const InviteScreen(),    
-  const ProfileScreen(), // <--- REPLACE THE PLACEHOLDER HERE
-];
-
+    const HomeDashboard(),   
+    const MiningDashboard(), 
+    const InviteScreen(),    
+    const ProfileScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1A1A2E),
-      body: _screens[_selectedIndex], // Now this will work
+      body: _screens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xFF16213E),
-        currentIndex: _selectedIndex, // Now this will work
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index; // Now this will work
-          });
-        },
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
         type: BottomNavigationBarType.fixed,
         selectedItemColor: const Color(0xFFE94560),
         unselectedItemColor: Colors.grey,
